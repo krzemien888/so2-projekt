e8fe73e83eb470ac9ade75b60299c1640d9abdfe
Black <freedomandjustit@gmail.com>
2018-05-14 18:53:30 +0200

    Adding presets and setup

---
+++ cmake-build-debug/CMakeFiles/so2_projekt.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/black/Work/krzem/main.cpp" "/home/black/Work/krzem/cmake-build-debug/CMakeFiles/so2_projekt.dir/main.cpp.o"
+  "/home/black/Work/krzem/utils/State.cpp" "/home/black/Work/krzem/cmake-build-debug/CMakeFiles/so2_projekt.dir/utils/State.cpp.o"
+  "/home/black/Work/krzem/utils/StateRaport.cpp" "/home/black/Work/krzem/cmake-build-debug/CMakeFiles/so2_projekt.dir/utils/StateRaport.cpp.o"
+  "/home/black/Work/krzem/views/IView.cpp" "/home/black/Work/krzem/cmake-build-debug/CMakeFiles/so2_projekt.dir/views/IView.cpp.o"
+  "/home/black/Work/krzem/views/RawConsoleMainView.cpp" "/home/black/Work/krzem/cmake-build-debug/CMakeFiles/so2_projekt.dir/views/RawConsoleMainView.cpp.o"
+  "/home/black/Work/krzem/workers/BaseSeparator.cpp" "/home/black/Work/krzem/cmake-build-debug/CMakeFiles/so2_projekt.dir/workers/BaseSeparator.cpp.o"
+  "/home/black/Work/krzem/workers/BaseWorker.cpp" "/home/black/Work/krzem/cmake-build-debug/CMakeFiles/so2_projekt.dir/workers/BaseWorker.cpp.o"
+  "/home/black/Work/krzem/workers/WoodchopperWorker.cpp" "/home/black/Work/krzem/cmake-build-debug/CMakeFiles/so2_projekt.dir/workers/WoodchopperWorker.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/so2_projekt.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/so2_projekt.dir/main.cpp.o"
+  "CMakeFiles/so2_projekt.dir/views/RawConsoleMainView.cpp.o"
+  "CMakeFiles/so2_projekt.dir/workers/BaseWorker.cpp.o"
+  "CMakeFiles/so2_projekt.dir/utils/State.cpp.o"
+  "CMakeFiles/so2_projekt.dir/utils/StateRaport.cpp.o"
+  "CMakeFiles/so2_projekt.dir/views/IView.cpp.o"
+  "CMakeFiles/so2_projekt.dir/workers/BaseSeparator.cpp.o"
+  "CMakeFiles/so2_projekt.dir/workers/WoodchopperWorker.cpp.o"
+  "so2_projekt.pdb"
+  "so2_projekt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/so2_projekt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
